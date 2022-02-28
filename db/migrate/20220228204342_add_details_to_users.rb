e89950e9694cb94ff8aeb4b955c16b67335deb4b
KingAlex1997 <alex97l@hotmail.com>
2022-02-28 18:53:41 -0300

    controller

--- db/migrate/20220228204342_add_details_to_users.rb
+++ db/migrate/20220228204342_add_details_to_users.rb
@@ -1,6 +1,6 @@
 class AddDetailsToUsers < ActiveRecord::Migration[6.1]
   def change
-    add_column :users, :address, :string
+    add_column :users, :adress, :string
     add_column :users, :fullname, :string
   end
 end
