25a2953e0ec77e2277c4ef7a65276fb5750e9a51
KingAlex1997 <alex97l@hotmail.com>
2022-03-09 13:43:23 -0300

    arreglo del toggle de index

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,14 +14,3 @@ ActiveStorage.start();
 
 import "controllers";
 import "bootstrap";
-
-const menuToggle = document.querySelector(".toggle");
-const showcase = document.querySelector(".showcase");
-
-if (menuToggle) {
-  menuToggle.addEventListener("click", () => {
-    menuToggle.classList.toggle("active");
-    showcase.classList.toggle("active");
-  });
-}
-
