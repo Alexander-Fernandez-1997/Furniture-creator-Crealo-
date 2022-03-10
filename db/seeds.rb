e585b3150946db5e7600e2e8df57da205c078570
Santiago Gaviola <santijgaviola@gmail.com>
2022-03-10 19:06:42 -0300

    three.js

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,3 +18,5 @@ Texture.create(name: "Black", url: "https://www.miliashop.com/img/co/105989.jpg"
 Texture.create(name: "Wood", url: "https://sc01.alicdn.com/kf/HTB1_wOuJVXXXXXiaXXXq6xXFXXXy/47811/HTB1_wOuJVXXXXXiaXXXq6xXFXXXy.jpg" )
 
 Texture.create(name: "Red", url: "https://i.pinimg.com/originals/05/b1/0d/05b10df54d54ebd95a86300df6baa4f0.jpg" )
+
+Texture.create(name: "Green", url: "https://assets.hongkiat.com/uploads/wood-textures/blue-wood.jpg" )
