32e03b6ab486aa422851d210b06fffc488075894
diego ripari <diego_ripari@hotmail.com>
2022-03-10 18:32:44 -0300

    views de devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   include Pundit::Authorization
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
   # Pundit: white-list approach.
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
@@ -14,6 +15,12 @@ class ApplicationController < ActionController::Base
   # end
 
   private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[fullname])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[fullname])
+  end
+
 
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
